5a132ed6b7ac7e2f7424b3e28d3b06dcf02c9d2f
JFo <88628933+JFoArlas@users.noreply.github.com>
2021-12-16 15:14:27 -0700

    skill drill

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,23 +2,26 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([39.8283, -98.5795], 4);
 
 // Coordinates for each point to be used in the polyline.
 let line = [
-    [33.9416, -118.4085],
     [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
   ]; 
 
 // Create a polyline using the line coordinates and make the line yellow.
 L.polyline(line, {
-    color: "yellow"
+    color: "blue",
+    weight: 4,
+    opacity: .5,
+    dashArray: '20'
  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
